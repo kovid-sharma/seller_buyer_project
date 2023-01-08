5449ab24a1c2739c2a5f5f10d01fcc58ff60334b
kovid-sharma <76726175+kovid-sharma@users.noreply.github.com>
2023-01-09 03:08:49 +0530

    detailsupdate

--- lib/details.dart
+++ lib/details.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
-
-
+import 'package:url_launcher/url_launcher.dart';
+//
+// final Uri _url = Uri.parse('https://flutter.dev');
+// Future<void> _launchUrl() async {
+//   if (!await launchUrl(_url)) {
+//     throw 'Unable to link to prject github $_url';
+//   }
+// }
 class details extends StatefulWidget {
   const details({Key? key}) : super(key: key);
 
@@ -17,6 +23,28 @@ class _detailsState extends State<details> {
           centerTitle: true,
           backgroundColor: Colors.black,
         ),
+        body: Column(
+          children: [
+            Container(
+
+              child: Text(
+                  'here details will be updated.',
+              ),
+            ),
+            // Center(
+            //   child: ElevatedButton(
+            //       style: ElevatedButton.styleFrom(
+            //         backgroundColor: Colors.black,
+            //       ),
+            //       onPressed: _launchUrl,
+            //       child: Text('Show Project On Github')
+            //
+            //   ),
+            // ),
+          ],
+        ),
+
     );
+
   }
 }
