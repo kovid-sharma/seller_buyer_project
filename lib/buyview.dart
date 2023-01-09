c20f2819a9b63d71fd3389597bfa4e57095a3f1b
kovid-sharma <76726175+kovid-sharma@users.noreply.github.com>
2023-01-09 11:14:50 +0530

    import

--- lib/buyview.dart
+++ lib/buyview.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:images_picker/images_picker.dart';
 class buyview extends StatefulWidget {
   const buyview({Key? key}) : super(key: key);
 
--- lib/selload.dart
+++ lib/selload.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:images_picker/images_picker.dart';
 class sellload extends StatefulWidget {
   const sellload({Key? key}) : super(key: key);
 
@@ -16,7 +16,51 @@ class _sellloadState extends State<sellload> {
         centerTitle: true,
         backgroundColor: Colors.black,
       ),
+      body: Column(
+        children: [
+          Expanded(child:
+            Text('Upload Files for selling',
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),)
+          ),
+          Expanded(
+          flex: 8,
+          child: Container(
 
+      ),
+    ),
+          Expanded(
+            flex: 8,
+            child: Container(
+
+          ),
+          ),
+
+          FloatingActionButton(
+          onPressed: ()
+      {
+
+      },
+      backgroundColor: Colors.greenAccent,
+            child: Text('+',
+              style: TextStyle(
+                fontSize: 40,
+              ),
+            ),
+          ),
+          Expanded(
+
+              flex: 1,
+              child: Container(
+
+              )
+          ),
+        ],
+
+
+        ),
     );
   }
 }
