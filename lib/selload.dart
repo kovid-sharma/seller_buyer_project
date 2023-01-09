ef97b78e9e446207b893160d3f0eaddf4d0b5b75
kovid-sharma <76726175+kovid-sharma@users.noreply.github.com>
2023-01-09 20:27:19 +0530

    sellLoadingFin

--- lib/selload.dart
+++ lib/selload.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 class sellload extends StatefulWidget {
   const sellload({Key? key}) : super(key: key);
@@ -14,7 +15,13 @@ class sellload extends StatefulWidget {
 class _sellloadState extends State<sellload> {
   @override
   Widget build(BuildContext context) {
+    TextEditingController _controllerName = TextEditingController();
+    TextEditingController _controllerQuantity = TextEditingController();
     String VidecurrUrl = '';
+
+
+    CollectionReference _allVideos =
+    FirebaseFirestore.instance.collection('shopping_list');
     return Scaffold(
       appBar: AppBar(
         title: Text('Left Brain'),
@@ -84,7 +91,15 @@ class _sellloadState extends State<sellload> {
         Reference refroot= FirebaseStorage.instance.ref();//this is the firebase storage
         Reference refchild= refroot.child('allVideos');
         Reference currVideo= refchild.child(curr);
-        currVideo.putFile(File(video!.path));
+        try {
+          await currVideo.putFile(File(video!.path));
+
+          VidecurrUrl= await currVideo.getDownloadURL();
+        }
+        catch(e)
+        {
+
+        }
     },
     ),
     SpeedDialChild(
@@ -117,6 +132,44 @@ class _sellloadState extends State<sellload> {
     ],
     ),
     floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      bottomNavigationBar: Container(
+        child: ElevatedButton(
+          onPressed:() async
+          {
+            if (VidecurrUrl.isEmpty) {
+              ScaffoldMessenger.of(context)
+                  .showSnackBar(SnackBar(content: Text('Not Successful Upload')));
+
+              return;
+            }
+            String prodName = _controllerName.text;
+            if(prodName.isEmpty){
+              ScaffoldMessenger.of(context)
+                  .showSnackBar(SnackBar(content: Text('No Name Provided')));
+            }
+            String Quant = _controllerQuantity.text;
+            if(Quant.isEmpty)
+              {
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(SnackBar(content: Text('No Quantity Provided')));
+              }
+            Map<String, String> dataToSend = {
+              'name': prodName,
+              'quantity': Quant,
+              'VideoUrl': VidecurrUrl,
+            };
+
+            _allVideos.add(dataToSend);
+
+            //Add a new item
+
+          },
+          child: Text('Submit File',),
+            style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.black,
+        ),
+        ),
+      ),
     );
   }
 }
